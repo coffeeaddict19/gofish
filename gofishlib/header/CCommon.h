1ecd76400c3ab75413aca28b97129f43a5bbf63b
John Laird <johnlaird45@gmail.com>
2021-04-02 11:21:26 -0500

    added defines for cards

--- gofishlib/header/CCommon.h
+++ gofishlib/header/CCommon.h
@@ -23,6 +23,67 @@ enum eTurnResult{
 #define kNumberOfCardsInBook 4
 #define kNumberOfCardsToDealPerPlayer 5
 
+/*
+          ACE   2   3   4   5   6   7   8   9   10   J   Q   K
+Clubs     0     4   8   12  16  20  24  28  32  36   40  44  48
+Diamonds  1     5   9   13  17  21  25  29  33  37   41  45  49
+Hearts    2     6   10  14  18  22  26  30  34  38   42  46  50
+Spades    3     7   11  15  19  23  27  31  35  39   43  47  51
+*/
+
+#define CARD_A_CLUB 0
+#define CARD_A_DIAMOND 1
+#define CARD_A_HEART 2
+#define CARD_A_SPADE 3
+#define CARD_2_CLUB 4
+#define CARD_2_DIAMOND 5
+#define CARD_2_HEART 6
+#define CARD_2_SPADE 7
+#define CARD_3_CLUB 8
+#define CARD_3_DIAMOND 9
+#define CARD_3_HEART 10
+#define CARD_3_SPADE 11
+#define CARD_4_CLUB 12
+#define CARD_4_DIAMOND 13
+#define CARD_4_HEART 14
+#define CARD_4_SPADE 15
+#define CARD_5_CLUB 16
+#define CARD_5_DIAMOND 17
+#define CARD_5_HEART 18
+#define CARD_5_SPADE 19
+#define CARD_6_CLUB 20
+#define CARD_6_DIAMOND 21
+#define CARD_6_HEART 22
+#define CARD_6_SPADE 23
+#define CARD_7_CLUB 24
+#define CARD_7_DIAMOND 25
+#define CARD_7_HEART 26
+#define CARD_7_SPADE 27
+#define CARD_8_CLUB 28
+#define CARD_8_DIAMOND 29
+#define CARD_8_HEART 30
+#define CARD_8_SPADE 31
+#define CARD_9_CLUB 32
+#define CARD_9_DIAMOND 33
+#define CARD_9_HEART 34
+#define CARD_9_SPADE 35
+#define CARD_10_CLUB 36
+#define CARD_10_DIAMOND 37
+#define CARD_10_HEART 38
+#define CARD_10_SPADE 39
+#define CARD_J_CLUB 40
+#define CARD_J_DIAMOND 41
+#define CARD_J_HEART 42
+#define CARD_J_SPADE 43
+#define CARD_Q_CLUB 44
+#define CARD_Q_DIAMOND 45
+#define CARD_Q_HEART 46
+#define CARD_Q_SPADE 47
+#define CARD_K_CLUB 48
+#define CARD_K_DIAMOND 49
+#define CARD_K_HEART 50
+#define CARD_K_SPADE 51
+
 enum ePlayers{
   John = 0,
   Dan = 1,
